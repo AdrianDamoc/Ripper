57d96a4617aa5eb47017391b6dac31fe41ae2fb9
AdrianDamoc <knifeofficial@gmail.com>
2019-12-17 12:51:40 +0200

    Defacut

---
+++ app/src/main/java/com/knifeapps/ripper/MainActivity.java
@@ -0,0 +1,36 @@
+package com.knifeapps.ripper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+    AutoCompleteTextView Denumire;
+    CalendarView Calendar;
+    Button BTNSave,BTNNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Denumire = findViewById(R.id.spinner_denumire);
+        Calendar = findViewById(R.id.calendar_id);
+        BTNSave = findViewById(R.id.btnsave);
+        BTNNext = findViewById(R.id.btnnext);
+
+        BTNNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Activitate2.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
